8c7c55406754e2f1110ac3d509625a7541530e37
kmamamori <kmamamori@miners.utep.edu>
2020-12-20 01:19:59 -0700

    Format code5.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
-
 // $( document ).ready(function() {
-//     var typing = new Typing({
-//         source: document.getElementById('source'),
-//         output: document.getElementById('output'),
-//         delay: 120,
-//         done: function() {
-// 	      console.log(this);
-// 	      console.log('done')
-// 	    }
-//     });
-    
-//     typing.start();
+//     console.log( "ready!" );
 // });
+
+$( document ).ready(function() {
+    var typing = new Typing({
+        source: document.getElementById('source'),
+        output: document.getElementById('output'),
+        delay: 120,
+        done: function() {
+	      console.log(this);
+	      console.log('done')
+	    }
+    });
+    
+    typing.start();
+});
 
