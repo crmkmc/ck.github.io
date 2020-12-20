29779615b8c0933373dd938ea8a28a2042531ccd
kmamamori <kmamamori@miners.utep.edu>
2020-12-20 00:15:41 -0700

    Format code.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 $( document ).ready(function() {
-    var typing = new Typing({
-        source: document.getElementById('source'),
-        output: document.getElementById('output'),
-        delay: 120,
-        done: function() {
-	      console.log(this);
-	      console.log('done')
-	    }
-    });
-    
-    typing.start();
+    console.log( "ready!" );
 });
+
+// $( document ).ready(function() {
+//     var typing = new Typing({
+//         source: document.getElementById('source'),
+//         output: document.getElementById('output'),
+//         delay: 120,
+//         done: function() {
+// 	      console.log(this);
+// 	      console.log('done')
+// 	    }
+//     });
+    
+//     typing.start();
+// });
 
