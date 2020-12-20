41722175e35d6d54c2d0b78525dda6c14fb717d3
kmamamori <kmamamori@miners.utep.edu>
2020-12-19 23:51:27 -0700

    Added jquery

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ $( document ).ready(function() {
     var typing = new Typing({
         source: document.getElementById('source'),
         output: document.getElementById('output'),
-        delay: 120
+        delay: 120,
+        done: function() {
+	      console.log(this);
+	      console.log('done')
+	    }
     });
     
     typing.start();
